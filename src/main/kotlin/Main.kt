80f6e39572698aa836bb779f40ad5586a921ba9a
Louis Cheung <dev.louisc@gmail.com>
2019-09-02 10:39:17 -0700

    Switch from Maven to Gradle

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,17 @@
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
+
 fun main(args: Array<String>) {
-    println("Hello, World!")
+    val options = Options()
+            .addOption(Option.builder()
+                    .argName("install")
+                    .desc("Install project dependencies")
+                    .build())
+
+    val cli = DefaultParser().parse(options, args)
+
+    if (cli.hasOption("install")) {
+        println("Command 'install' encountered!")
+    }
 }
--- src/test/kotlin/depmgmt/ProjectJsonParserTest.kt
+++ src/test/kotlin/depmgmt/ProjectJsonParserTest.kt
@@ -18,15 +18,15 @@ class ProjectJsonParserTest {
                 {
                     "groupId": "groupId",
                     "artifactId": "artifactId",
-                    "version": "1.0.0",
+                    "version": "0.0.0",
                     "dependencies": [
-                        "dependent-groupId:dependent-artifactId:2.0.0"
+                        "dependent-groupId:dependent-artifactId:1.0.0"
                     ],
                     "developmentDependencies": [
-                        "development-groupId:development-artifactId:3.0.0"
+                        "development-groupId:development-artifactId:2.0.0"
                     ],
                     "overridingDependencies": [
-                        "overriding-groupId:overriding-artifactId:4.0.0"
+                        "overriding-groupId:overriding-artifactId:3.0.0"
                     ]
                 }
             """.trimIndent()
@@ -37,10 +37,10 @@ class ProjectJsonParserTest {
             // THEN the resultant project JSON object is returned
             assertThat(projectJson.groupId).isEqualTo("groupId")
             assertThat(projectJson.artifactId).isEqualTo("artifactId")
-            assertThat(projectJson.version).isEqualTo("1.0.0")
-            assertThat(projectJson.dependencies).isEqualTo(listOf("dependent-groupId:dependent-artifactId:2.0.0"))
-            assertThat(projectJson.developmentDependencies).isEqualTo(listOf("development-groupId:development-artifactId:3.0.0"))
-            assertThat(projectJson.overridingDependencies).isEqualTo(listOf("overriding-groupId:overriding-artifactId:4.0.0"))
+            assertThat(projectJson.version).isEqualTo("0.0.0")
+            assertThat(projectJson.dependencies).isEqualTo(listOf("dependent-groupId:dependent-artifactId:1.0.0"))
+            assertThat(projectJson.developmentDependencies).isEqualTo(listOf("development-groupId:development-artifactId:2.0.0"))
+            assertThat(projectJson.overridingDependencies).isEqualTo(listOf("overriding-groupId:overriding-artifactId:3.0.0"))
         }
 
     }
